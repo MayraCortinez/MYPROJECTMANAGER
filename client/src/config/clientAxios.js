3ffd00b4c76353f59a6b3293ec6041d22597b248
MayC <mayrautopia@gmail.com>
2024-02-05 09:51:43 -0300

    fix clientAxios

--- client/src/config/clientAxios.js
+++ client/src/config/clientAxios.js
@@ -1,7 +1,4 @@
 import Axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Carga las variables de entorno desde .env
 
 const baseURL = process.env.NODE_ENV === 'production'
   ? process.env.VITE_URL_BACKEND_PROD // Usando la variable de entorno de producción
