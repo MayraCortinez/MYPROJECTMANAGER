438d599c1a808fe4983d2aecc525359b40640b0b
MayC <mayrautopia@gmail.com>
2024-02-05 03:36:07 -0300

    first commit pre deploy

--- client/src/config/clientAxios.js
+++ client/src/config/clientAxios.js
@@ -1,10 +1,11 @@
+import Axios from 'axios';
 
-import Axios from 'axios'
+const baseURL = process.env.NODE_ENV === 'production'
+  ? process.env.VITE_URL_BACKEND_PROD // Usando la variable de entorno de producción
+  : process.env.VITE_URL_BACKEND; // Usando la variable de entorno de desarrollo
 
+export const clientAxios = Axios.create({
+  baseURL: baseURL,
+});
 
-export const clientAxios = Axios.create( {
-    
-    baseURL : 'http://localhost:4000/api'
-
-  
-})
+export default clientAxios;
