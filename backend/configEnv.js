c18bc21898fbb09dc451d604785d72aace046055
MayC <mayrautopia@gmail.com>
2024-02-05 08:38:23 -0300

    deploy vercel

--- backend/configEnv.js
+++ backend/configEnv.js
@@ -8,7 +8,7 @@ const configEnv = {
 
   // Variable que cambia según el entorno
   BACKEND_URL: isProduction ? process.env.BACKEND_URL_PROD : process.env.BACKEND_URL,
-  FRONTENDURL: isProduction ? process.env.FRONTEND_URL_PROD : process.env.FRONTEND_URL,
+  FRONTEND_URL: isProduction ? process.env.FRONTEND_URL_PROD : process.env.FRONTEND_URL,
   VITE_URL_BACKEND: isProduction ? process.env.VITE_URL_BACKEND_PROD : process.env.VITE_URL_BACKEND,
 };
 
