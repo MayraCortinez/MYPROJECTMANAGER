60fb859fa62e5e7a35074113d03dc496fb7ca341
MayC <mayrautopia@gmail.com>
2024-02-05 10:32:13 -0300

    fix whiteList

--- backend/configEnv.js
+++ backend/configEnv.js
@@ -1,5 +1,3 @@
-// config.js
-
 require('dotenv').config(); // Cargar las variables de entorno desde .env
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -10,7 +8,10 @@ const configEnv = {
   FRONTEND_URL: process.env.FRONTEND_URL_PROD || process.env.FRONTEND_URL,
   VITE_URL_BACKEND: process.env.VITE_URL_BACKEND_PROD || process.env.VITE_URL_BACKEND,
 
-  whiteList: [configEnv.BACKEND_URL, configEnv.FRONTEND_URL], // Lista única para ambos entornos
+  whiteList: [
+    process.env.BACKEND_URL_PROD || process.env.BACKEND_URL,
+    process.env.FRONTEND_URL_PROD || process.env.FRONTEND_URL,
+  ], 
 };
 
 module.exports = configEnv;
