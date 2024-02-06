0ad0112d5c7c819c960c5f5ee4c1273f4451b655
MayC <mayrautopia@gmail.com>
2024-02-06 03:17:39 -0300

    fix configAxios urlBase

--- client/src/config/clientAxios.jsx
+++ client/src/config/clientAxios.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.MODE === 'production'
-  ? import.meta.env.VITE_URL_BACKEND_PROD
-  : import.meta.env.VITE_URL_BACKEND;
+// const baseURL = import.meta.env.MODE === 'production'
+//   ? import.meta.env.VITE_URL_BACKEND_PROD
+//   : import.meta.env.VITE_URL_BACKEND;
 
 export const clientAxios = axios.create({
-  baseURL,
+  baseURL : 'https://backend-my-project-manager.onrender.com/'
 });
