1519138c22156f8259055eeb62d4801c457dfec8
MayC <mayrautopia@gmail.com>
2024-02-05 22:52:08 -0300

    add get /

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,6 @@ const { register, verifyToken, login, changePassword, checked, sendToken } = req
 /* /api/auth */
 
 router
-    .get('/', login)
     .post('/register', register )
     .post('/login', login )
     .get('/checked', checked )
@@ -14,5 +13,9 @@ router
     .route('/reset-password')
         .get(verifyToken)
         .post(changePassword)
+
+    router.get('/', (req, res) => {
+            res.send('Bienvenido a la API del proyecto manager'); // Puedes personalizar este mensaje
+    });
 
 module.exports = router;
