ef724d76deb71bb020885a3b9836635978f2f402
MayC <mayrautopia@gmail.com>
2024-02-06 02:21:45 -0300

    fix environment

--- backend/helpers/errorResponse.js
+++ backend/helpers/errorResponse.js
@@ -2,6 +2,6 @@ module.exports = (res, error, method) => {
     console.log(error);
     return res.status (error.status || 500).json({
         ok: false,
-        msg: error.message || `Existen problemas con ${method}`
+        msg: error.message && `Existen problemas con ${method}`
     })
 }
