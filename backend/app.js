7a8caea372c27187f3bcfc7abed1f5ee0a822ce1
MayC <mayrautopia@gmail.com>
2024-02-07 20:29:27 -0300

    add url backend & frontend

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app
   .use(express.urlencoded({ extended: false }))
   
   const corsOptions = {
-    origin: 'https://my-project-manager-frontend.onrender.com', // El origen permitido
+    origin: 'https://client-mdhirr5xo-mayracortinez.vercel.app/', // El origen permitido
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Los métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Los encabezados permitidos
     credentials: true // Si se permiten credenciales
