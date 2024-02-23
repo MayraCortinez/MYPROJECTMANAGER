ced7a1b8355fad3c74ddeadfa29b09f29032145a
MayC <mayrautopia@gmail.com>
2024-02-23 15:56:13 -0300

    add react-cookie vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import reactCookie from 'react-cookie';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [reactCookie], 
+    },
+  },
   server: {
     proxy: {
       '/api': 'https://backend-kappa-one-37.vercel.app',
