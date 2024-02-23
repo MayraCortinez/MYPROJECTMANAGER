2ae6e9769fd2e98b26738f836b5cfcdd4ae5a1ef
MayC <mayrautopia@gmail.com>
2024-02-23 17:41:07 -0300

    fix authControllers

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactCookie from 'react-cookie';
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
